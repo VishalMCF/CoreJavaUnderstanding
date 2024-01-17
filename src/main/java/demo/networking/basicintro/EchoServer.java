9f9a4517d3094ba5e61323f41a1a6b98fa674675
VishalMCF <70262922+VishalMCF@users.noreply.github.com>
2024-01-17 13:30:10 +0530

    created packages for java networking study

---
+++ src/main/java/demo/networking/basicintro/EchoServer.java
@@ -0,0 +1,11 @@
+package demo.networking.basicintro;
+
+public class EchoServer {
+
+    public static void main(String[] args) {
+        System.out.println("Simple Echo Server");
+
+        // serversocket is the class which is used by the server to listen to the client requests
+        // its argument is the port number
+    }
+}
